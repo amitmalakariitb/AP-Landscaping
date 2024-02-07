eb73024f54cdd09860f193750a538a42c2bf05c4
Amit <amitmalakar887@gmail.com>
2024-02-07 21:12:46 +0530

    Changed the button css

--- app.js
+++ app.js
@@ -17,14 +17,20 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.post('/send-email', (req, res) => {
-    console.log(req)
     const { userContact } = req.body;
+
+    const userContactFormatted = `
+    Name: ${userContact.name}
+    Email: ${userContact.email}
+    Phone: ${userContact.phone}
+    Message: ${userContact.message}
+    `;
 
     const msg = {
         to: 'malakaramiit@gmail.com',
         from: process.env.SENDER_EMAIL,
         subject: 'Contact Information',
-        text: `Contact Information\n\nUser Details:\n${userContact}`,
+        text: `Contact Information\n\nUser Details:\n${userContactFormatted}`,
     };
 
 
