93114e9db9e04dbf1463e26eabb044270f4558f9
Amit <amitmalakar887@gmail.com>
2024-02-14 23:55:11 +0530

    TESTING email once again

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.post('/send-email', (req, res) => {
     const { name, email, phone, message } = req.body.userContact;
 
     const msg = {
-        to: 'Asingh@sbconsultantllc.com',
+        to: 'malakaramiit@gmail.com',
         from: process.env.SENDER_EMAIL,
         subject: 'Contact Information',
         text: `User Details:\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
